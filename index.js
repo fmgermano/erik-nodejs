0b2e4cf6da15cd89a23b79cb4a4066b37c2f05c1
Felipe Mateus <fmgermano@gmail.com>
2022-04-25 17:21:25 -0300

    refatoracao com promises

---
+++ index.js
@@ -0,0 +1,128 @@
+
+ /*
+ 0 - Obter um usuario
+ 1 - Obter o numero de telefone de um usuario a partir de seu ID
+ 2 - Obter o endereço do usuario pelo Id
+*/ 
+// importamos um modulo interno do node.js
+const util = require('util')
+const obterEnderecoAsync = util.promisify(obterEndereco)
+
+
+function obterUsuario() {
+  // quando der algum problema -> chama reject e passsa o erro
+// quando sucess -> resolv
+  return new Promise(function resolvePromise(resolve,reject) {
+   
+      setTimeout(function () {
+        //  return reject(new Error('DEU RUIM DE VERDADE!'))
+        
+        return resolve({
+          id: 1,
+          nome: 'Aladin',
+          dataNascimento : new Date()
+        })
+      },1000)
+        
+  })
+  
+
+}   
+ 
+function obterTelefone(idUsuario) {
+  return new Promise(function resolverPromise(resolve,reject) {
+    setTimeout (() => {
+      return resolve({
+        telefone : '11001212',
+        ddd: 11
+      })
+    }, 2000);
+    
+  })
+
+
+}
+
+function obterEndereco (idUsuario,callback) {
+  setTimeout(() => {
+    return callback(null, {
+      rua: 'dos bobos',
+      numero: 0
+    })
+  },2000);
+
+}
+
+const usuarioPromise = obterUsuario()
+//para manipular o sucesso , usamos a funcao .ten
+// p manipular erro,usamos o .catch
+usuarioPromise
+.then(function (usuario) {
+  return obterTelefone(usuario.id)
+  .then(function resolverTelefone(result) {
+    return {
+      usuario: {
+        nome: usuario.nome,
+        id: usuario.id
+      },
+      telefone: result
+    }
+  })
+})
+.then(function (resultado){
+  const endereco = obterEnderecoAsync(resultado.usuario.id)
+  return endereco.then(function resolverEndereco(result) {
+    return {
+      usuario: resultado.usuario,
+      telefone: resultado.telefone,
+      endereco: result
+    }
+  })
+})
+.then(function(resultado) {
+  console.log(`
+    Nome: ${resultado.usuario.nome}
+    Endereço: ${resultado.endereco.rua}.${resultado.endereco.numero}
+    Telefone: (${resultado.telefone.ddd}) ${resultado.telefone.telefone}
+  `)
+})
+.catch(function (error) {
+  console.error(' deu ruim', error)
+})
+
+
+
+
+
+
+// refatoração
+/* function resolverUsuario(erro,usuario) {
+  console.log('usuario', usuario)
+
+}
+
+ obterUsuario(function resolverUsuario(error,usuario ) {
+   // VALOR NULO,VAZIO,ZERO =>  null || '' || 0 === false
+   if(error) {
+     console.error('Deu ruim em usuario', error)
+     return;
+   }
+   obterTelefone(usuario.id, function resolverTelefone(error1,telefone) {
+    if(error1) {
+      console.error('Deu ruim em Telefone', error)
+      return;
+    }
+    obterEndereco(usuario.id, function resolverEndereco(error2,endereco) {
+      if(error2) {
+        console.error('Deu ruim em usuario', error)
+        return;
+      }
+      console.log(`
+      Nome: ${usuario.nome},
+      Endereco: ${endereco.rua}, ${endereco.numero},
+      Telefone: (${telefone.ddd}),${telefone.telefone}
+      `)
+    })
+ })
+})
+ */
